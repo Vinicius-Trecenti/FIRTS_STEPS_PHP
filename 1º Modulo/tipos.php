4fcf331cef358d0a357e8e7dea66c354c4392245
Vinicius <vinitrecente5@gmail.com>
2023-11-26 21:30:42 -0300

    trabalhando com tipos em PHP

---
+++ 1º Modulo/tipos.php
@@ -0,0 +1,20 @@
+<?php 
+
+$idade =21;
+
+//double  tem mais precisao
+$salario = 1000.30;
+
+//float é mais simples 
+//tipo da variavel nome valor
+
+$divisao = 10/3;
+
+//textos - string
+$texto = "Ola mundo";
+
+//booleano
+$verdadeiro = true;
+$falso = false;
+
+echo gettype($falso);

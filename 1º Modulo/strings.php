9379ef24ea2bdc172d8b14f7c41f5c42fe134a9f
Vinicius <vinitrecente5@gmail.com>
2023-11-27 15:30:44 -0300

    strings - concatenando

---
+++ 1º Modulo/strings.php
@@ -0,0 +1,16 @@
+<?php
+
+ $idade = 20;
+
+ //utiliza-se . ponto para exibir o conteudo de um texto mais outra varialvel
+ echo "Minha idade é: " . $idade . " anos ";
+
+ $email = "vinitrecenti@gmail.com";
+
+ echo 'Meu email é: ' . $email;
+
+ //Para aspas simples precisa de ponto (.) para aspas duplas o PHP entende
+ echo "meu email é $email";
+
+ 
+//para quebrar linhas em php

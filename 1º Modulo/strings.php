ee0ecb497b927aabe5948d4b6a379026cc716c59
Vinicius <vinitrecente5@gmail.com>
2023-11-27 15:52:56 -0300

    Funcao php_EOL

--- 1º Modulo/strings.php
+++ 1º Modulo/strings.php
@@ -3,14 +3,21 @@
  $idade = 20;
 
  //utiliza-se . ponto para exibir o conteudo de um texto mais outra varialvel
- echo "Minha idade é: " . $idade . " anos ";
+ //echo "Minha idade é: " . $idade . " anos ";
 
  $email = "vinitrecenti@gmail.com";
 
- echo 'Meu email é: ' . $email;
+ //echo 'Meu email é: ' . $email;
 
  //Para aspas simples precisa de ponto (.) para aspas duplas o PHP entende
- echo "meu email é $email";
+ //echo "meu email é $email";
 
  
-//para quebrar linhas em php
+//para quebrar linhas em php - \n
+//para tab \t
+
+//echo "meu email é: $email \nminha idade é: $idade com teste de \t tab";
+
+//podemos usar a quebra de linha do proprio PHP PHP_EOL
+echo "Olá mundo" . PHP_EOL;
+echo "meu email é: $email \nminha idade é: $idade com teste de \t tab";

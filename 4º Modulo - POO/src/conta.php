05b7e477b8f0b80942a9e7cbd1bf76190a0e39dc
Vinicius <vinitrecente5@gmail.com>
2024-01-03 12:17:31 -0300

    POO

---
+++ 4º Modulo - POO/src/conta.php
@@ -0,0 +1,10 @@
+<?php
+
+function criarConta(string $cpf, string $nomeTitular, float $saldo): array{
+    return [
+        $cpf => [
+            'titular' => $nomeTitular,
+            'saldo' => $saldo
+        ]
+    ];
+}

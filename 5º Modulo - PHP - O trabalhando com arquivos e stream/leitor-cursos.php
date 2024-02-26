b6ab6927d9ae893127294e4df8d84bf1fbe04eed
Vinicius <vinitrecente5@gmail.com>
2024-02-26 16:56:03 -0300

    lendo o arquivo apenas uma vez

--- 5º Modulo - PHP - O trabalhando com arquivos e stream/leitor-cursos.php
+++ 5º Modulo - PHP - O trabalhando com arquivos e stream/leitor-cursos.php
@@ -3,11 +3,10 @@
 //abrindo o arquivo
 $arquivo = fopen('lista-cursos.txt', 'r');
 
-//lendo o arquivo
-while(!feof($arquivo)) {
-    //lendo uma linha
-    $linha = fgets($arquivo);
-    echo $linha ;
-}   
+//ler o arquivo como um todo
+
+$lendo = fread($arquivo, filesize('lista-cursos.txt'));
+echo $lendo;
+
 //para fechar o arquivo e eos outros programas possam usar - boa pratica 
 fclose($arquivo);

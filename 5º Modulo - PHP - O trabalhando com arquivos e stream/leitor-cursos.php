058ac0f8f080c29886fbc2b6b095d54c5590408f
Vinicius Trecenti <vinitrecenti@gmail.com>
2024-04-10 15:40:39 -0300

    funcao get contents

--- 5º Modulo - PHP - O trabalhando com arquivos e stream/leitor-cursos.php
+++ 5º Modulo - PHP - O trabalhando com arquivos e stream/leitor-cursos.php
@@ -1,12 +1,15 @@
 <?php
 
 //abrindo o arquivo
-$arquivo = fopen('lista-cursos.txt', 'r');
+// $arquivo = fopen('lista-cursos.txt', 'r');
 
-//ler o arquivo como um todo
+// //ler o arquivo como um todo
 
-$lendo = fread($arquivo, filesize('lista-cursos.txt'));
-echo $lendo;
+// $lendo = fread($arquivo, filesize('lista-cursos.txt'));
+// echo $lendo;
 
-//para fechar o arquivo e eos outros programas possam usar - boa pratica 
-fclose($arquivo);
+// //para fechar o arquivo e eos outros programas possam usar - boa pratica 
+// fclose($arquivo);
+
+$cursos = file_get_contents('lista-cursos.txt');
+echo $cursos;

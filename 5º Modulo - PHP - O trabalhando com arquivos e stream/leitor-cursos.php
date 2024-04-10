80b0920f52812c2b0b8b78e1d64d4a55ef4a26b3
Vinicius Trecenti <vinitrecenti@gmail.com>
2024-04-10 15:42:29 -0300

    funcao file tras como array

--- 5º Modulo - PHP - O trabalhando com arquivos e stream/leitor-cursos.php
+++ 5º Modulo - PHP - O trabalhando com arquivos e stream/leitor-cursos.php
@@ -11,5 +11,5 @@
 // //para fechar o arquivo e eos outros programas possam usar - boa pratica 
 // fclose($arquivo);
 
-$cursos = file_get_contents('lista-cursos.txt');
-echo $cursos;
+$cursos = file('lista-cursos.txt');
+var_dump($cursos);

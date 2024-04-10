eb8d3c4fd97ec2a550d8948e13325bef7ffebd5e
Vinicius Trecenti <vinitrecenti@gmail.com>
2024-04-10 15:56:29 -0300

    criando, lendo e escrevendo

---
+++ 5º Modulo - PHP - O trabalhando com arquivos e stream/escritor-cursos.php
@@ -0,0 +1,17 @@
+<?php
+
+//modo de leitura a ele tenta criar se o arquivo não existir
+// se existir ele escreve no final
+//modos de leitura e escrito
+// r - leitura
+// w - escrita
+// a - escrita no final
+// x - criação
+// + - leitura e escrita
+// consultar documentacao do fopen()
+$arquivo = fopen('cursos-php.txt', 'a');
+$cursos = "\n CURSO DE ARQUIVOS COM PHP 3";
+
+// NO ARQUIVO SERA ESCRICO CURSOS PODENDO INFORMAR QUANTOS CARACTERES
+fwrite($arquivo, $cursos );
+fclose($arquivo); 
